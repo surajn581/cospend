c2608be0830233d920cea73e0cdc4132a0808cf6
surajn581 <surajn581@gmail.com>
2026-02-08 01:42:21 +0530

    initial impl of a input parser that returns infow and outflow nodes given an input string of transactions

---
+++ transaction_parser.py
@@ -0,0 +1,50 @@
+from abc import abstractmethod
+import numpy as np
+NOT = '!'
+
+class TransactionInputParser:
+    
+    @classmethod
+    @abstractmethod
+    def parse(cls, allTransactions:str) -> tuple[list, list]:
+        pass
+
+class StringTransactionInputParser(TransactionInputParser):
+    
+    @classmethod
+    def parse(cls, allTransactions:str) -> tuple[list, list]:
+        # reading the first line and getting the names of all the group members
+        flowMap = {node: 0 for node in allTransactions.lower().strip().split('\n')[0].split(' ')[1:]}
+
+        # iterating over each line starting from the 2nd line
+        for transaction in allTransactions.lower().strip().split('\n')[1:]:
+            lender, amount, *borrowers = transaction.split(' ')
+
+            # if no borrowers are named then it implies that the amount is to be
+            # split equally amongst the whole group
+            if not borrowers:
+                borrowers = list(flowMap)
+
+            # if borrowers are listed with ! in the beginning of their names then
+            # the amount has to be split equally amongst everyone in the group
+            # except the names listed with !
+            elif NOT in transaction:
+                borrowers = list( set(flowMap).difference(set(b[1:] for b in borrowers)) )
+
+            amount = np.round(float(amount)/len(borrowers))
+            lenderInflowAmount = 0
+            for borrower in borrowers:
+                if borrower == lender:
+                    continue
+                lenderInflowAmount += amount
+                flowMap[borrower] += amount
+            flowMap[lender] += -lenderInflowAmount
+
+        outflowNodes = {node: value for node, value in flowMap.items() if value>0}
+        inflowNodes = {node: value for node, value in flowMap.items() if value<0}
+
+        inflowNodes = list(inflowNodes.items())
+        outflowNodes = list(outflowNodes.items())
+
+        return inflowNodes, outflowNodes
+
