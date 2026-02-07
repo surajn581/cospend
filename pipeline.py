ef297220493004af1c7cb1cbe95e40c3238561b0
surajn581 <surajn581@gmail.com>
2026-02-08 02:53:35 +0530

    Using the logTranscations API to log transactions of the settlement results & returning the results

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 from lib.logging import logger
 from transaction_parser import TransactionInputParser, StringTransactionInputParser
+from settlement_result import SettlementResult
 import settle
 
 class SettlementPipeline:
@@ -10,11 +11,12 @@ class SettlementPipeline:
         self.inputParser = inputParser
         self.nParticipantsThreshold = nParticipantsThreshold
 
-    def _getSettlementTransactions(self, inputTransactions):
+    def _getSettlementTransactions(self, inputTransactions:str)->SettlementResult:
         inflow, outflow = self.inputParser.parse(inputTransactions)
         return settle.Settler(nParticipantsThreshold=self.nParticipantsThreshold).settle(inflow, outflow)
     
     def getSettlementTransactions(self, inputTransactions):
-        transactions = self._getSettlementTransactions(inputTransactions)
-        for borrower, lender, amount in sorted(transactions, key = lambda row: (row[0], -row[-1]) ):
-            logger.info(f'{borrower.capitalize()} has to pay {lender.capitalize()} {amount}')
+        result = self._getSettlementTransactions(inputTransactions)
+        result.logTransactions()
+        return result
+        
